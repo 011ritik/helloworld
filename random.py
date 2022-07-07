dfe1de279336fc896b24d7d85c686998dd4da9ce
RitiKSiBJr <letter.ritik060@gmail.com>
2022-07-07 10:25:35 +0545

    first commit

---
+++ random.py
@@ -0,0 +1,22 @@
+
+import random
+
+min_value = 0
+max_value = 9
+
+print(random.randrange(1,9))
+
+i=1
+
+# length = int(input("Choose the length of the number you want to generate:\t"))
+
+
+# while length >= i :
+#     print(random.randrange(min_value,max_value))
+
+#     print (i)
+    
+#     i += 1
+
+
+
