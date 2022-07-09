6163dfba80010f5bfaab85e3fb619514741e7344
RitiKSiBJr <letter.ritik060@gmail.com>
2022-07-09 17:15:30 +0545

    commiting the file

---
+++ re.py
@@ -0,0 +1,25 @@
+
+import random
+
+min_value = 0
+max_value = 10
+
+#print(random.randrange(1,9))
+
+i=1
+
+
+length = int(input("Choose the length of the number you want to generate:\t"))
+
+while length >= i :
+    print(random.randrange(min_value,max_value))
+
+
+
+    
+    i += 1
+
+
+
+
+
